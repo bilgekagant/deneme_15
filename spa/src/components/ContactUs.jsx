21cda290d68e8ccf253894a22967fe37e3fd2d57
butkua123 <butkua@gmail.com>
2023-01-01 23:54:22 +0300

    style-changes

--- spa/src/components/ContactUs.jsx
+++ spa/src/components/ContactUs.jsx
@@ -12,11 +12,12 @@ export default function ContactUs (){
         <Container className='position-relative'>
             <form>
                 <FormGroup className='mb-3' controlId='formGroupEmail'>
-                    <FormLabel>Email Address</FormLabel>
+                    <FormLabel className="form-label">Email Address</FormLabel>
                     <Form.Control type='email' placeholder='Enter email' />
 
                 </FormGroup>
                 <FormGroup className='mb-3'>
+                    <FormLabel className="form-label">İsim Soyisim</FormLabel>
                     <Row>
                         <Col>
                             <Form.Control placeholder="First name" />
@@ -28,10 +29,12 @@ export default function ContactUs (){
                 </FormGroup>
 
                 <Form.Group as={Col} controlId="formGridState">
-                    <Form.Label>Konu seçiniz</Form.Label>
+                    <Form.Label className="form-label" >Konu seçiniz</Form.Label>
                         <Form.Select defaultValue="Konu seçiniz">
+                            <option>Enstruman Onarım</option>
                             <option>Enstruman Bakım</option>
                             <option>Enstruman Satış</option>
+                            <option>Kariyer Desteği</option>
                             <option>İstek/Öneri/Şikayet</option>
                         </Form.Select>
                 </Form.Group>
@@ -39,14 +42,13 @@ export default function ContactUs (){
                 <FormGroup className='mb-3' controlId='exampleForm.ControlTextarea1'>
                     <Row>
                         <Form.Label column="lg" lg={12}>
-                            Large Text
                         </Form.Label>
                         <Col>
                             <Form.Control as= "textarea" rows={3} placeholder="İletmek istediğiniz konuyu yazınız" />
                         </Col>
                     </Row>  
                 </FormGroup>
-                <Button className=""as="input" type="submit" value="Gönder" />{' '}
+                <button className= "submit-button">Gönder</button>
             </form>
 
         </Container>
--- spa/src/components/Services.jsx
+++ spa/src/components/Services.jsx
@@ -1,10 +1,57 @@
-import React from "react";
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-export default function Services(){
+
+const Cardd = () => {
     return(
+      <div className="row">
+        <div className="col-lg-6 col-md-6 col-sm-6">
+          <div class="card bg-dark text-white">
+            <div class="card-header mx-auto">
+              Onarım
+            </div>
+            <img class="card-img" src="Repair1_compressed.jpg" alt="Card image"/>
+          </div>
+        </div>
+        <div className="col-lg-6 col-md-6 col-sm-6">
+          <div class="card bg-dark text-white">
+            <div class="card-header mx-auto">
+            Bakım
+            </div>
+            <img class="card-img" src="Blog_Banner_1212x400_4_702104d6-fb80-4cd5-be9c-bfc5a55ffe46_1200x1200.webp" alt="Card image"/>
+          </div>
+        </div>
+        <div className="col-lg-6 col-md-6 col-sm-6">
+          <div class="card bg-dark text-white">
+            <div class="card-header mx-auto">
+            Satış
+            </div>
+            <img class="card-img" src="IMG_6907.jpg" alt="Card image"/>
+          </div>
+        </div>
+        <div className="col-lg-6 col-md-6 col-sm-6 mx-auto">
+          <div class="card bg-dark text-white">
+            <div class="card-header mx-auto">
+             Kariyer Desteği
+            </div>
+            <img class="card-img" src="pexels-david-yu-1749822.jpg" alt="Card image"/>
+          </div>
+        </div>
+      </div>
+      
+  
+    );
+  }
+
+
+export default function Services() {
+    return (
         <div>
-            <h1>These are our servise</h1>
-            <h2>like 31</h2>
+            <h1 className='mx-auto'class="instrument-pages-header">Servislerimiz</h1>
+            <div className="d-flex flex-row">
+                <div className="p-2 position-relative"><Cardd/></div>
+            </div>
         </div>
     )
 }
