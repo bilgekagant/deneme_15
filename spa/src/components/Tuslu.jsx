79f4925d76b0f86dc52ebcf2c7d9b901dfd782ff
butkua123 <butkua@gmail.com>
2023-01-02 01:47:47 +0300

    brands-and-design-update

--- spa/src/components/Tuslu.jsx
+++ spa/src/components/Tuslu.jsx
@@ -71,72 +71,75 @@ export default function Tuslu() {
             </div>
 
 
-            <Container className='row tusluElements' id='org'>
+            <Container className='row vurmaliElements' id='org'>
             <h2>Org Markalarımız</h2>
               <ul className='col-lg-6'>
                 <li>Yamaha</li>
-                <li>siksok</li>
-                <li>a3131</li>
+                <li>Hammond</li>
+                <li>Casio</li>
               </ul>
-              <p className='col-lg-6'>Bateri, davullar ve ziller başta olmak üzere vurmalı çalgılardan oluşan bir müzik enstrümanıdır. Şarkılarda ritim tutmaya yarar. Fransızcadan Türkçeye geçmiş bir kelimedir. Türkçede "bateri" yerine "davul", "davul seti" ve "davul kiti" terimleri de kullanılır.</p>
-              <img class="vurmaliImg" src="pexels-josh-sorenson-995301.jpg" alt="Card image"/>
+              <p className='col-lg-6 instrument-text'>Müzikte, org, her biri kendi el kitabından, ellerle veya ayaklarla pedallı tahta ile çalınan tonlar üretmek için bir veya daha fazla boru bölümünün veya başka araçların klavyeli bir enstrümanıdır.</p>
+              <img class="vurmaliImg" src="org-muzik.jpg" alt="Card image"/>
 
           </Container>
           <br></br>
-          <Container className='row tusluElements' id='kilise'>
+          <Container className='row vurmaliElements' id='kilise'>
             <h2>Kilise Orgu Markalarımız</h2>
               <ul className='col-lg-6'>
+                <li>Hammond</li>
+                <li>Roland</li>
                 <li>Yamaha</li>
-                <li>siksok</li>
-                <li>a3131</li>
+                <li>Phoenix</li>
               </ul>
-              <p className='col-lg-6'>Bendir, Klasik Türk müziğinde ve özellikle Mevlevi Türk Tasavvuf Musikisinde daire veya def adıyla bilinen vurmalı çalgının Mağrip ülkelerine (özellikle Fas ve Cezayir'e) özgü biçimidir. Mağrip Arapçasından alınan bendir adı Türkiye'de 1980'lerden sonra yaygın kullanıma kavuşmuştur.</p>
-              <img class="vurmaliImg" src="4.jpg" alt="Card image"/>
+              <p className='col-lg-6 instrument-text'>Borulu org, körükle verilen basınçlı havanın tahta veya metal borulardan geçerken içerideki hava sütunlarının titreşmesi sayesinde ses üreten bir müzik enstrümanıdır. Klavyeli ve pedallı bir enstrümandır. Değişik ses tonları ve notalar piyanoda olduğu gibi tuşlarla denetlenir. En çok bilinen türü kilise orgudur.</p>
+              <img class="vurmaliImg" src="pexels-tim-gouw-250695.jpg" alt="Card image"/>
           </Container>
           <br></br>
 
-          <Container className='row tusluElements' id='synthesizer'>
+          <Container className='row vurmaliElements' id='synthesizer'>
             <h2>Synthesizer Markalarımız</h2>
               <ul className='col-lg-6'>
-                <li>Yamaha</li>
-                <li>siksok</li>
-                <li>a3131</li>
+                <li>Moog Music</li>
+                <li>Korg</li>
+                <li>Arturia</li>
               </ul>
-              <p className='col-lg-6'>Bongo, bir vurmalı çalgı türüdür. Güney Amerika ve Afrika doğumlu olan bongo, Küba müziğinde 19. yüzyıldan sonra görüldü. 1940'larda yapılan metal türler daha sonra geliştirildi.</p>
-              <img class="vurmaliImg" src="pexels-skynig-photography-9488150.jpg" alt="Card image"/>
+              <p className='col-lg-6 instrument-text'>Synthesizer farklı türde bir müzik yaratmak ve elektriksel sinyaller üretmek için kullanılan bir müzik aletidir. Üretilen sinyaller bir enstrüman amfisi, hoparlör ya da kulaklık aracılığıyla sese dönüştürülüp duyulur hale gelir.</p>
+              <img class="vurmaliImg" src="synths-9.6.2016.jpg" alt="Card image"/>
           </Container>
           <br></br>
-          <Container className='row tusluElements' id='akordeon'>
+          <Container className='row vurmaliElements' id='akordeon'>
             <h2>Akordeon Markalarımız</h2>
               <ul className='col-lg-6'>
-                <li>Yamaha</li>
-                <li>siksok</li>
-                <li>a3131</li>
+                <li>Hohner</li>
+                <li>Borsini</li>
+                <li>Weltmeister</li>
               </ul>
-              <p className='col-lg-6'>Marimba, vurma çalgılar ailesinden tuşlu bir çalgıdır. Genellikle tahtadan yapılma olan tuşlara malletlerle vurularak ses elde edilir. 4 1/3 - 5 1/2 oktav ses alanına sahiptir. Tuşlar piyanodaki gibi düzenlenmiştir. Kromatik marimba Güney Meksika ve Kuzey Guatemala'daki diatonic marimbadan türedi.</p>
-              <img class="vurmaliImg" src="Marimba.webp" alt="Card image"/>
+              <p className='col-lg-6 instrument-text'>Akordeon, akordiyon ya da akordion, bir körüğü harekete geçirmekle yaratılan hava akımının etkilediği serbest metal dillerinin titreşmesiyle ses çıkaran havalı çalgıdır. Bir ya da iki kılavuz ile bir körükten oluşan akordiyonda, serbest metal dillerin titreşmesi, klavyenin tuşlarına basmakla sağlanır.</p>
+              <img class="vurmaliImg" src="pexels-pixabay-163823.jpg" alt="Card image"/>
           </Container>
           <br></br>
-          <Container className='row tusluElements' id='piyano'>
+          <Container className='row vurmaliElements' id='piyano'>
             <h2>Piyano Markalarımız</h2>
               <ul className='col-lg-6'>
-                <li>Yamaha</li>
-                <li>siksok</li>
-                <li>a3131</li>
+                <li>Steinway & Sons </li>
+                <li>Bӧsendorfer</li>
+                <li>Blüthner</li>
+                <li>Bechstein </li>
+                <li>Yamaha </li>
               </ul>
-              <p className='col-lg-6'>Bu üçgen çalgı üstündeki köşede ilmik yapılmış bir çeşit katgut, ip veya tel ile havada tutulabilir. Genellikle kısa bir metal değnek şeklinde tokmak ile vurularak çalınır. Bir vuruş yüksek, tiz tınlayan bir zil çalar notası verir. İstenirse bu tınlama çalgı üzerine elle dokunularak kontrol edilebilir.</p>
-              <img class="vurmaliImg" src="il_570xN.3428363159_jdg6.webp" alt="Card image"/>
+              <p className='col-lg-6 instrument-text'>Piyano, tellere daha yumuşak bir malzeme ile kaplanmış tahta çekiçlerle vurulan telli klavyeli bir enstrümandır. Oyuncunun, çekiçlerin tellere çarpmasını sağlamak için iki elin parmakları ve başparmaklarıyla bastırdığı veya vurduğu bir dizi tuş olan bir klavye kullanılarak oynanır.</p>
+              <img class="vurmaliImg" src="pexels-pixabay-164743.jpg" alt="Card image"/>
           </Container>
           <br></br>
-          <Container className='row tusluElements' id='keytar'>
+          <Container className='row vurmaliElements' id='keytar'>
             <h2>Keytar Markalarımız</h2>
               <ul className='col-lg-6'>
                 <li>Yamaha</li>
                 <li>siksok</li>
                 <li>a3131</li>
               </ul>
-              <p className='col-lg-6'>Davul, bilinen en eski vurmalı çalgılardan biridir. Ahşap, maden ya da pişmiş topraktan silindirik bir gövdeye gerilen deriden oluşur. El ya da sopayla çalınır. Biçimi değişse de dünyanın her yerinde ve her toplumda kullanılan bir çalgıdır. Türkçede davulun diğer adları; köbürge, küvgür, tuğ, tavul, tabıl dır.</p>
-              <img class="vurmaliImg" src="akp-li-belediye-ramazan-davulunu-yasakladi-997671-5.jpg" alt="Card image"/>
+              <p className='col-lg-6 instrument-text'>Keytar, gitarın kayışla desteklenmesine benzer şekilde, boyun ve omuzların etrafındaki bir kayışla desteklenen hafif bir synthesizer'dır. Keytarlar, sabit sehpalar üzerine yerleştirilen geleneksel klavyelere kıyasla oyunculara sahnede daha geniş bir hareket alanı sağlar.</p>
+              <img class="vurmaliImg" src="FRMYBW0JDOUWRMX.webp" alt="Card image"/>
           </Container>
 
 
