7cda9e7874fc6cd0defee334dfeeab496d7b813a
bilgekagant <84462943+bilgekagant@users.noreply.github.com>
2023-01-02 03:41:41 +0300

    gereksiz importlar

--- spa/src/components/Tuslu.jsx
+++ spa/src/components/Tuslu.jsx
@@ -1,6 +1,3 @@
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/esm/Container';
 
 const Cardd = () => {
